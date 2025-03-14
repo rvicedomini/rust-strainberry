a4c6f76f75227f78d808c1eba0b07ac6c98ed816
rvicedomini <ric.vice@protonmail.com>
2025-03-14 20:32:57 +0100

    started proper implementation of the HaploGraph structure to possibly merge adjacent phased haplotypes

--- src/phase/haplograph.rs
+++ src/phase/haplograph.rs
@@ -1,70 +1,171 @@
-use std::fs::File;
-use std::io::{BufWriter, Write};
-use std::path::Path;
+// use std::fs::File;
+// use std::io::{BufWriter, Write};
+// use std::path::Path;
 
 use itertools::Itertools;
-use rustc_hash::FxHashMap;
+use rustc_hash::{FxHashMap, FxHashSet};
+use tinyvec::{tiny_vec, TinyVec};
 
-use super::haplotype::Haplotype;
+use crate::utils::BamRecordId;
+use super::haplotype::{HaplotypeId, Haplotype};
 
-type HaplotypeId = (usize,usize,usize,usize);
+#[derive(Debug, Default, Clone)]
+struct Edge(HaplotypeId,usize);
+
+type AdjList = FxHashMap<HaplotypeId,TinyVec<[Edge;10]>>;
 
 pub struct HaploGraph {
-    graph: FxHashMap<HaplotypeId,Vec<(HaplotypeId,usize)>>
+    succ: AdjList,
+    pred: AdjList,
 }
 
 impl HaploGraph {
 
-    pub fn new(haplotypes: &FxHashMap<HaplotypeId,Haplotype>, sread_haplotypes: &FxHashMap<String, Vec<HaplotypeId>>) -> HaploGraph {
-        let mut graph = FxHashMap::default();
+    pub fn new(haplotypes: &FxHashMap<HaplotypeId,Haplotype>, sread_haplotypes: &FxHashMap<BamRecordId, Vec<HaplotypeId>>) -> HaploGraph {
+        let mut succ: AdjList = AdjList::default();
+        let mut pred: AdjList = AdjList::default();
         
         for ht_uid in haplotypes.keys() {
-            graph.insert(*ht_uid, vec![]);
+            succ.insert(*ht_uid, tiny_vec!());
+            pred.insert(*ht_uid, tiny_vec!());
         }
 
         for ht_list in sread_haplotypes.values() {
-            for (a,b) in ht_list.iter().sorted_unstable_by_key(|&hid| haplotypes[hid].beg()).tuple_windows::<(_,_)>() {
-                let a_adj = graph.get_mut(a).unwrap();
-                if let Some(idx) = a_adj.iter().position(|(uid,_)| uid == b) {
-                    let p = &mut a_adj[idx];
-                    p.1 += 1;
+            for (a,b) in ht_list.iter().sorted_unstable_by_key(|&hid| haplotypes[hid].beg()).tuple_windows() {
+                let a_succ = succ.get_mut(a).unwrap();
+                if let Some(idx) = a_succ.iter().position(|Edge(uid,_)| uid == b) {
+                    a_succ[idx].1 += 1;
                 } else {
-                    a_adj.push((*b,1));
+                    a_succ.push(Edge(*b,1));
+                }
+                let b_pred = pred.get_mut(b).unwrap();
+                if let Some(idx) = b_pred.iter().position(|Edge(uid,_)| uid == a) {
+                    b_pred[idx].1 += 1;
+                } else {
+                    b_pred.push(Edge(*a,1));
                 }
             }
         }
 
-        HaploGraph{graph}
+        HaploGraph{ succ, pred }
     }
 
-    pub fn write_dot(&self, path:&Path) -> std::io::Result<()> {
+    fn out_degree(&self, hid:&HaplotypeId) -> usize { self.succ[hid].len() }
+    fn in_degree(&self, hid:&HaplotypeId) -> usize { self.pred[hid].len() }
 
-        let file = File::create(path)?;
-        let mut dot = BufWriter::new(file);
+    pub fn remove_weak_edges(&mut self, min_reads:usize, min_frac:f64) {
 
-        dot.write_all(b"digraph \"\" {\n")?;
-        dot.write_all(b"\tgraph [rankdir=LR, splines=true];\n")?;
-
-        dot.write_all(b"\tnode [label=\"\\N\"];\n")?;
-
-        for node in self.graph.keys() {
-            let node_line = format!("\t\"{node:?}\" [style=filled, fillcolor=white];\n");
-            dot.write_all(node_line.as_bytes())?;
-        }
-
-        for (a, succ) in self.graph.iter() {
-            let a_name = format!("{a:?}");
-            for (b,cnt) in succ.iter() {
-                let b_name = format!("{b:?}");
-                let edge_line = format!("\t\"{a_name}\" -> \"{b_name}\" [label={cnt}];\n");
-                dot.write_all(edge_line.as_bytes())?;
+        let mut deleted_edges = FxHashSet::default();
+        for node in self.succ.keys() {
+            if self.out_degree(node) > 0 {
+                let out_reads: usize = self.succ[node].iter().map(|&Edge(_,n)| n).sum();
+                self.succ[node].iter()
+                    .filter(|Edge(_,edge_reads)| *edge_reads < min_reads || (*edge_reads as f64)/(out_reads as f64) < min_frac)
+                    .for_each(|Edge(succ_id,_)| {
+                        deleted_edges.insert((*node,*succ_id));
+                    });
+            }
+            if self.in_degree(node) > 0 {
+                let in_reads: usize = self.pred[node].iter().map(|&Edge(_,n)| n).sum();
+                self.pred[node].iter()
+                    .filter(|Edge(_,edge_reads)| *edge_reads < min_reads || (*edge_reads as f64)/(in_reads as f64) < min_frac)
+                    .for_each(|Edge(pred_id,_)| {
+                        deleted_edges.insert((*pred_id,*node));
+                    });
             }
         }
 
-        dot.write_all(b"}\n")?;
+        // TODO: possibly create method for the following loop (also done at the end of this function)
+        for (a,b) in deleted_edges.drain() {
+            self.succ.get_mut(&a).unwrap().retain(|Edge(succ,_)| succ != &b);
+            self.pred.get_mut(&b).unwrap().retain(|Edge(pred,_)| pred != &a);
+        }
 
-        Ok(())
+        let mut phased_map: FxHashMap<(usize,usize),TinyVec<[HaplotypeId;10]>> = FxHashMap::default();
+        for hid in self.succ.keys() {
+            phased_map.entry((hid.beg, hid.end))
+                .or_default()
+                .push(*hid);
+        }
+
+        let sorted_regions = phased_map.keys().cloned().sorted();
+        for (prev,succ) in sorted_regions.tuple_windows() {
+            let nb_edges: usize = phased_map[&prev].iter().map(|hid| self.out_degree(hid)).sum();
+            let prev_nodes: usize = phased_map[&prev].len();
+            let succ_nodes: usize = phased_map[&succ].len();
+            if nb_edges != succ_nodes || prev_nodes != succ_nodes {
+                deleted_edges.extend(phased_map[&prev].iter()
+                    .flat_map(|a| {
+                        self.succ.get(a).unwrap().iter()
+                            .map(move |Edge(b,_)| (*a,*b))
+                    })
+                );
+            }
+        }
+
+        // TODO: possibly create method for the following loop
+        for (a,b) in deleted_edges {
+            self.succ.get_mut(&a).unwrap().retain(|Edge(succ,_)| succ != &b);
+            self.pred.get_mut(&b).unwrap().retain(|Edge(pred,_)| pred != &a);
+        }
     }
+
+    pub fn scaffold_haplotypes(&mut self, variant_positions:&FxHashSet<(usize,usize)>, min_reads:usize, min_frac:f64) -> FxHashMap<HaplotypeId,Haplotype> {
+        assert!(min_frac > 0.5);
+
+        self.remove_weak_edges(min_reads, min_frac);
+        
+        // assert(all(self.hg.out_degree(n) < 2 and self.hg.in_degree(n) < 2 for n in self.hg.nodes))
+        // scaffolds = []
+        // haplotype_scaffold = dict()
+        // for ht_from in self.hg.nodes:
+        //     if self.hg.in_degree(ht_from) == 0:
+        //         haplotype_nodes = self._scaffold_from(ht_from)
+        //         ht_name = haplotype_nodes.pop()
+        //         scf = copy.deepcopy(self.name2haplotype[ht_name])
+        //         haplotype_scaffold[ht_name] = scf
+        //         while len (haplotype_nodes) > 0:
+        //             ht_name = haplotype_nodes.pop()
+        //             scf.extend(self.name2haplotype[ht_name])
+        //             haplotype_scaffold[ht_name] = scf
+        //         scf.trim(variant_positions,0)
+        //         assert(scf.size() > 0)
+        //         if scf.size() >= 3:
+        //             scaffolds.append(scf)
+        // return scaffolds
+
+
+        todo!()
+    }
+
+    // pub fn write_dot(&self, path:&Path) -> std::io::Result<()> {
+
+    //     let file = File::create(path)?;
+    //     let mut dot = BufWriter::new(file);
+
+    //     dot.write_all(b"digraph \"\" {\n")?;
+    //     dot.write_all(b"\tgraph [rankdir=LR, splines=true];\n")?;
+
+    //     dot.write_all(b"\tnode [label=\"\\N\"];\n")?;
+
+    //     for node in self.graph.keys() {
+    //         let node_line = format!("\t\"{node:?}\" [style=filled, fillcolor=white];\n");
+    //         dot.write_all(node_line.as_bytes())?;
+    //     }
+
+    //     for (a, succ) in self.graph.iter() {
+    //         let a_name = format!("{a:?}");
+    //         for (b,cnt) in succ.iter() {
+    //             let b_name = format!("{b:?}");
+    //             let edge_line = format!("\t\"{a_name}\" -> \"{b_name}\" [label={cnt}];\n");
+    //             dot.write_all(edge_line.as_bytes())?;
+    //         }
+    //     }
+
+    //     dot.write_all(b"}\n")?;
+
+    //     Ok(())
+    // }
 }
 
 
