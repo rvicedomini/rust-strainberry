f037dff0e25cc97e2631fd3248c8c3972cce21de
rvicedomini <ric.vice@protonmail.com>
2025-03-24 18:58:26 +0100

    fixed critical bug preventing to properly map reads to strain-aware contigs

--- src/awarecontig.rs
+++ src/awarecontig.rs
@@ -161,7 +161,7 @@ pub fn map_alignments_to_aware_contigs(alignments: &[SeqAlignment], aware_contig
         let sa_id = sa.record_id();
 
         let idx = aware_contigs.partition_point(|ctg| ctg.tid() < sa.tid() || (ctg.tid() == sa.tid() && ctg.end() <= sa.target_beg()));
-        for (i,ctg) in aware_contigs[idx..].iter().take_while(|ctg| ctg.beg() < sa.target_end()).enumerate() {
+        for (i,ctg) in aware_contigs[idx..].iter().take_while(|ctg| ctg.tid() == sa.tid() && ctg.beg() < sa.target_end()).enumerate() {
             
             if ctg.is_phased() && (!seq2haplo.contains_key(&sa_id) || !seq2haplo[&sa_id].contains(&ctg.haplotype_id().unwrap())) {
                 continue
