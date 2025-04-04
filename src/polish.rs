d40fa7bde8172ddc29ebf03a571881133cba9b98
rvicedomini <ric.vice@protonmail.com>
2025-04-04 19:01:08 +0200

    refactor and implementation of two custom polishing modes

--- src/polish.rs
+++ src/polish.rs
@@ -9,17 +9,14 @@ use ahash::AHashMap as HashMap;
 use crate::cli::Options;
 use crate::seq::alignment::{MappingType, PafAlignment};
 
+#[derive(Debug, PartialEq)]
+pub enum PolishMode {
+    Aware,
+    Oblivious,
+}
 
-fn filter_paf_alignments() {}
 
-pub fn racon_polish(target_path: &Path, read_path: &Path, work_dir: PathBuf, opts: &Options) -> Result<PathBuf> {
-
-    if which::which("racon").is_err() {
-        bail!("missing racon dependency, please check your system PATH");
-    }
-
-    std::fs::create_dir_all(&work_dir)
-        .with_context(|| format!("cannot create output directory: \"{}\"", work_dir.display()))?;
+fn run_minimap2(target_path: &Path, read_path: &Path, work_dir: &Path, mode: PolishMode, opts: &Options) -> Result<PathBuf> {
 
     // Run minimap2 and load alignments
 
@@ -40,10 +37,24 @@ pub fn racon_polish(target_path: &Path, read_path: &Path, work_dir: PathBuf, opt
     let reader = BufReader::new(stdout);
     reader.lines()
         .map_while(Result::ok)
-        .map(|line| line.trim().to_string())
-        .filter(|line| !line.is_empty())
-        .map(|line| PafAlignment::from_str(&line).with_context(||format!("error parsing line:\n{line}")).unwrap())
-        .filter(|paf| matches!(paf.map_type(10, 0.01), MappingType::DovetailPrefix|MappingType::DovetailSuffix|MappingType::QueryContained|MappingType::ReferenceContained))
+        .filter_map(|line| {
+            let line = line.trim();
+            if !line.is_empty() { Some(line.to_string()) } else { None }
+        })
+        .map(|line| {
+            PafAlignment::from_str(&line)
+                .with_context(||format!("error parsing line:\n{line}"))
+                .unwrap()
+        })
+        .filter(|paf| {
+            match mode {
+                PolishMode::Aware => matches!(
+                    paf.map_type(10, 0.01),
+                    MappingType::DovetailPrefix|MappingType::DovetailSuffix|MappingType::QueryContained|MappingType::ReferenceContained
+                ),
+                PolishMode::Oblivious => true,
+            }
+        })
         .for_each(|paf| {
             let qname = paf.query_name.clone();
             if !alignments.contains_key(&qname) {
@@ -56,31 +67,41 @@ pub fn racon_polish(target_path: &Path, read_path: &Path, work_dir: PathBuf, opt
 
     // Write retained alignments in PAF format
 
-    let paf_path = {
-        let paf_path = work_dir.join("alignments.paf.gz");
-        let mut paf_writer = crate::utils::get_file_writer(&paf_path);
-        for paf in alignments.into_values() {
-            paf_writer.write_all(paf.to_string().as_bytes())?;
-            paf_writer.write_all(b"\n")?;
-        }
-        paf_path
-    };
+    let paf_path = work_dir.join("alignments.paf.gz");
+    let mut paf_writer = crate::utils::get_file_writer(&paf_path);
+    for paf in alignments.into_values() {
+        paf_writer.write_all(paf.to_string().as_bytes())?;
+        paf_writer.write_all(b"\n")?;
+    }
+
+    Ok(paf_path)
+}
+
+pub fn racon_polish(target_path: &Path, read_path: &Path, out_path: &Path, mode: PolishMode, work_dir: &Path, opts: &Options) -> Result<()> {
+
+    if which::which("racon").is_err() {
+        bail!("missing racon dependency, please check your system PATH");
+    }
+
+    std::fs::create_dir_all(work_dir)
+        .with_context(|| format!("cannot create output directory: \"{}\"", work_dir.display()))?;
+
+    let paf_path = run_minimap2(target_path, read_path, work_dir, mode, opts)?;
 
     // Run racon
 
-    let polished_path = work_dir.join("polished.fasta");
-    let polished_file = std::fs::File::create(&polished_path).unwrap();
+    let out_file = std::fs::File::create(out_path).unwrap();
     let racon_log_path = work_dir.join("racon.log");
     let racon_log_file = std::fs::File::create(&racon_log_path).unwrap();
 
     let args = ["-t", &opts.nb_threads.to_string(), "-u", read_path.to_str().unwrap(), paf_path.to_str().unwrap(), target_path.to_str().unwrap()];
     // println!("running cmd: racon {}", args.join(" "));
     let mut racon = Command::new("racon").args(args)
-        .stdout(Stdio::from(polished_file))
+        .stdout(Stdio::from(out_file))
         .stderr(Stdio::from(racon_log_file))
         .spawn().context("cannot run racon")?;
     
     racon.wait().unwrap();
 
-    Ok(polished_path)
+    Ok(())
 }
