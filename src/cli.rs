b46747d52f848807c263baed02bc29f28b246a21
rvicedomini <ric.vice@protonmail.com>
2025-04-26 20:10:29 +0200

    set default min-aware-ctg-len to 2k

--- src/cli.rs
+++ src/cli.rs
@@ -94,7 +94,7 @@ pub struct Options {
     pub min_overhang: usize,
 
     /// Minimum aware-contig length
-    #[arg(long = "min-aware-ctg-len", value_name = "NUM", default_value_t = 3000)]
+    #[arg(long = "min-aware-ctg-len", value_name = "NUM", default_value_t = 2000)]
     pub min_aware_ctg_len: usize,
 
     /// Minimum aware-contig length
