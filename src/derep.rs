0dca1d78ad32a382b06a06e871379cd08de72683
rvicedomini <ric.vice@protonmail.com>
2025-05-10 21:50:00 +0200

    testing a simpler de-replication approach

--- src/derep.rs
+++ src/derep.rs
@@ -12,7 +12,7 @@ use itertools::Itertools;
 use needletail::Sequence;
 
 use crate::cli::Options;
-use crate::seq::alignment::{MappingType, PafAlignment};
+use crate::seq::alignment::PafAlignment;
 
 
 type MatchInterval = (usize,usize,String);
@@ -43,50 +43,59 @@ fn compute_matching_intervals(fasta_path: &Path, opts: &Options) -> Result<HashM
 
     let mut matching_intervals: HashMap<String,Vec<(usize,usize,String)>> = HashMap::new();
     for a in alignments.into_iter().filter(|a| a.query_name != a.target_name) {
-        let map_type = a.map_type(1000, 0.8);
+        
+        // let map_type = a.map_type(100, 0.5);
         assert!(a.mapping_length > 0);
-        let identity = (a.matches as f64)/(a.mapping_length as f64);
-        // println!("{a} => {map_type:?} / {identity:.2}");
-        if identity < 0.90 {
+        
+        // if a.mapping_length < 1000 && !map_type.is_containment() {
+        //     continue
+        // }
+
+        if (a.query_end-a.query_beg).min(a.target_end-a.target_beg) < 2000 || a.identity < 90.0 {
             continue
         }
-        match map_type {
-            MappingType::QueryContained => {
-                matching_intervals.entry(a.query_name).or_default().push((0,a.query_length,a.target_name));
-            },
-            MappingType::QueryPrefix => {
-                let (query_beg, query_end) = if a.strand == b'+' { (0,a.query_end) } else { (a.query_beg,a.query_length) };
-                // println!("{} => {map_type:?} | {} | ({},{},{})", a.query_name, a.strand as char, query_beg, query_end, a.target_name);
-                matching_intervals.entry(a.query_name).or_default().push((query_beg, query_end,a.target_name));
-            },
-            MappingType::QuerySuffix => {
-                let (query_beg, query_end) = if a.strand == b'+' { (a.query_beg,a.query_length) } else { (0,a.query_end) };
-                // println!("{} => {map_type:?} | {} | ({},{},{})", a.query_name, a.strand as char, query_beg, query_end, a.target_name);
-                matching_intervals.entry(a.query_name).or_default().push((query_beg, query_end,a.target_name));
-            },
-            MappingType::ReferenceContained => {
-                matching_intervals.entry(a.target_name).or_default().push((0,a.target_length,a.query_name));
-            },
-            MappingType::ReferencePrefix => {
-                // println!("{} => {map_type:?} | {} | ({},{},{})", a.target_name, a.strand as char, 0,a.target_end, a.query_name);
-                matching_intervals.entry(a.target_name).or_default().push((0,a.target_end,a.query_name));
-            },
-            MappingType::ReferenceSuffix => {
-                // println!("{} => {map_type:?} | {} | ({},{},{})", a.target_name, a.strand as char, a.target_beg,a.target_length, a.query_name);
-                matching_intervals.entry(a.target_name).or_default().push((a.target_beg,a.target_length,a.query_name));
-            },
-            MappingType::DovetailPrefix => {
-                let (query_beg, query_end) = if a.strand == b'+' { (0,a.query_end) } else { (a.query_beg,a.query_length) };
-                matching_intervals.entry(a.query_name.clone()).or_default().push((query_beg, query_end,a.target_name.clone()));
-                matching_intervals.entry(a.target_name).or_default().push((a.target_beg, a.target_length, a.query_name));
-            },
-            MappingType::DovetailSuffix => {
-                let (query_beg, query_end) = if a.strand == b'+' { (a.query_beg,a.query_length) } else { (0,a.query_end) };
-                matching_intervals.entry(a.query_name.clone()).or_default().push((query_beg, query_end,a.target_name.clone()));
-                matching_intervals.entry(a.target_name).or_default().push((0, a.target_end, a.query_name));
-            },
-            MappingType::Internal => {}
-        }
+
+        // let (query_beg, query_end) = if a.strand == b'+' { (a.query_beg,a.query_end) } else { (a.query_beg,a.query_length) };
+        matching_intervals.entry(a.query_name.clone()).or_default().push((a.query_beg, a.query_end, a.target_name.clone()));
+        matching_intervals.entry(a.target_name).or_default().push((a.target_beg, a.target_length, a.query_name));
+
+        // match map_type {
+        //     MappingType::QueryContained => {
+        //         matching_intervals.entry(a.query_name).or_default().push((0,a.query_length,a.target_name));
+        //     },
+        //     MappingType::QueryPrefix => {
+        //         let (query_beg, query_end) = if a.strand == b'+' { (0,a.query_end) } else { (a.query_beg,a.query_length) };
+        //         // println!("{} => {map_type:?} | {} | ({},{},{})", a.query_name, a.strand as char, query_beg, query_end, a.target_name);
+        //         matching_intervals.entry(a.query_name).or_default().push((query_beg, query_end,a.target_name));
+        //     },
+        //     MappingType::QuerySuffix => {
+        //         let (query_beg, query_end) = if a.strand == b'+' { (a.query_beg,a.query_length) } else { (0,a.query_end) };
+        //         // println!("{} => {map_type:?} | {} | ({},{},{})", a.query_name, a.strand as char, query_beg, query_end, a.target_name);
+        //         matching_intervals.entry(a.query_name).or_default().push((query_beg, query_end,a.target_name));
+        //     },
+        //     MappingType::ReferenceContained => {
+        //         matching_intervals.entry(a.target_name).or_default().push((0,a.target_length,a.query_name));
+        //     },
+        //     MappingType::ReferencePrefix => {
+        //         // println!("{} => {map_type:?} | {} | ({},{},{})", a.target_name, a.strand as char, 0,a.target_end, a.query_name);
+        //         matching_intervals.entry(a.target_name).or_default().push((0,a.target_end,a.query_name));
+        //     },
+        //     MappingType::ReferenceSuffix => {
+        //         // println!("{} => {map_type:?} | {} | ({},{},{})", a.target_name, a.strand as char, a.target_beg,a.target_length, a.query_name);
+        //         matching_intervals.entry(a.target_name).or_default().push((a.target_beg,a.target_length,a.query_name));
+        //     },
+        //     MappingType::DovetailPrefix => {
+        //         let (query_beg, query_end) = if a.strand == b'+' { (0,a.query_end) } else { (a.query_beg,a.query_length) };
+        //         matching_intervals.entry(a.query_name.clone()).or_default().push((query_beg, query_end,a.target_name.clone()));
+        //         matching_intervals.entry(a.target_name).or_default().push((a.target_beg, a.target_length, a.query_name));
+        //     },
+        //     MappingType::DovetailSuffix => {
+        //         let (query_beg, query_end) = if a.strand == b'+' { (a.query_beg,a.query_length) } else { (0,a.query_end) };
+        //         matching_intervals.entry(a.query_name.clone()).or_default().push((query_beg, query_end,a.target_name.clone()));
+        //         matching_intervals.entry(a.target_name).or_default().push((0, a.target_end, a.query_name));
+        //     },
+        //     MappingType::Internal => {}
+        // }
     }
 
     Ok(matching_intervals)
@@ -96,7 +105,6 @@ fn compute_matching_intervals(fasta_path: &Path, opts: &Options) -> Result<HashM
 pub fn derep_assembly(fasta_path: &Path, work_dir: &Path, opts: &Options) -> Result<PathBuf> {
     assert!(fasta_path.is_file());
 
-    // TODO: delete temporary directory at the end
     let tmp_dir = work_dir.join("tmp");
     std::fs::create_dir_all(&tmp_dir)
         .with_context(|| format!("Cannot create output directory: \"{}\"", tmp_dir.display()))?;
@@ -121,16 +129,19 @@ pub fn derep_assembly(fasta_path: &Path, work_dir: &Path, opts: &Options) -> Res
             seq_dict.insert(name, sequence);
         }
 
+        let mut derep_ctg_id: usize = 0;
         let derep_path = tmp_dir.join(format!("derep_{it}.fasta"));
         let mut derep_writer = crate::utils::get_file_writer(&derep_path);
         let mut processed: HashSet<&str> = HashSet::new();
-        for (seq_id, sequence) in seq_dict.iter().sorted_unstable_by_key(|(_,seq)| seq.len()) {
-            let mut seq_intervals = vec![(0,sequence.len())];
+        for (seq_id, sequence) in seq_dict.iter().sorted_by_key(|(_,seq)| seq.len()) {
+            
+            let mut seq_intervals = Vec::new();
+            
             if let Some(intervals) = matching_intervals.get(seq_id.as_str()) {
                 let sorted_intervals = intervals.iter()
                     .filter_map(|(beg,end,name)| {
                         if !processed.contains(name.as_str()) { Some((*beg,*end,name.clone())) } else { None }
-                    }).sorted_unstable().collect_vec();
+                    }).sorted().collect_vec();
                 
                 let mut merged_intervals = Vec::with_capacity(sorted_intervals.len());
                 for (beg,end,_) in sorted_intervals {
@@ -144,39 +155,29 @@ pub fn derep_assembly(fasta_path: &Path, work_dir: &Path, opts: &Options) -> Res
                 }
 
                 modified = modified || !merged_intervals.is_empty();
-                for iv in &merged_intervals {
-                    assert!(iv.0 < iv.1);
-                    let first = seq_intervals.partition_point(|(_,end)| *end <= iv.0);
-                    let last = seq_intervals.partition_point(|(beg,_)| *beg < iv.1 );
-                    let overlapping = seq_intervals.drain(first..last).collect_vec();
-                    for (beg,end) in overlapping {
-                        if iv.0 <= beg && end <= iv.1 {
-                            continue
-                        } else if beg <= iv.0 && iv.1 <= end {
-                            if beg < iv.0 { seq_intervals.push((beg,iv.0)); }
-                            if iv.1 < end { seq_intervals.push((iv.1,end)); }
-                        } else if beg < iv.0 {
-                            assert!(end < iv.1);
-                            seq_intervals.push((beg,iv.0))
-                        } else {
-                            assert!(iv.0 < beg && iv.1 < end);
-                            seq_intervals.push((iv.1,end));
-                        }
-                    }
+
+                let mut beg = 0;
+                for &(mi_beg, mi_end) in &merged_intervals {
+                    seq_intervals.push((beg, mi_beg));
+                    beg = mi_end;
                 }
+                seq_intervals.push((beg, sequence.len()));
                 processed.insert(seq_id.as_str());
+            } else { // no matching interval: output the full sequence
+                seq_intervals.push((0,sequence.len()));
             }
 
             // Sequences are processed in increasing length and only alignments involving the suffix/prefix are retained
             // This means I cannot get more than 1 interval
-            assert!(seq_intervals.len() <= 1);
+            // assert!(seq_intervals.len() <= 1);
             
-            for (beg,end) in seq_intervals {
-                let header = format!(">{seq_id}\n");
+            for &(beg,end) in seq_intervals.iter().filter(|(beg,end)| end-beg >= 2000) {
+                let header = format!(">{derep_ctg_id}\n");
                 derep_writer.write_all(header.as_bytes())?;
                 let sequence = crate::utils::insert_newlines(&sequence[beg..end], 120);
                 derep_writer.write_all(sequence.as_bytes())?;
                 derep_writer.write_all(b"\n")?;
+                derep_ctg_id += 1;
             }
         }
 
